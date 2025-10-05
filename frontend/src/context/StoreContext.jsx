e3d8f3ca0b7f1e2c39294a7eca37c862b73a7d96
aswinen-17 <enaswin17@gmail.com>
2025-10-05 19:37:31 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://home-service-booking-platform.onrender.com"
     const [token, setToken] = useState("");
     const [service_list,setServiceList] = useState([])
 
